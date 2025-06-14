7118a132f05b983584436e3737ba79f02c7a649f
CaffeineNazar <140259313+CaffeineNazar@users.noreply.github.com>
2025-06-14 11:15:46 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
             'Muhammad Nazarul Iman',
             style: TextStyle(color: Colors.white),
             ),
+            centerTitle: true,
           backgroundColor: Colors.blueAccent,
         ),
         body: Center(
